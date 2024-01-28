228165f1dd49f17102b945a8b07af43ffe58c9dc
Aghakarim-dev <hemidzade1010@gmail.com>
2024-01-29 00:47:31 +0400

    test

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+
+    <div className='flex justify-center flex-wrap gap-10'>
+      <div className='bg-blue-200 transition-all hover:shadow-2xl hover:border border-black'>
+      <div className='w-[350px] h-[250px] bg-blue-300 flex justify-center items-center hover:text-[32px] border border-black hover:scale-95  transition-all		'>1</div>
+      </div>
+      <div className='bg-blue-200 transition-all hover:shadow-2xl hover:border border-black'>
+      <div className='w-[350px] h-[250px] bg-blue-300 flex justify-center items-center hover:text-[32px] border border-black hover:scale-95  transition-all		'>2</div>
+      </div>
+      <div className='bg-blue-200 transition-all hover:shadow-2xl hover:border border-black'>
+      <div className='w-[350px] h-[250px] bg-blue-300 flex justify-center items-center hover:text-[32px] border border-black hover:scale-95  transition-all		'>3</div>
+      </div>
+      <div className='bg-blue-200 transition-all hover:shadow-2xl hover:border border-black'>
+      <div className='w-[350px] h-[250px] bg-blue-300 flex justify-center items-center hover:text-[32px] border border-black hover:scale-95  transition-all		'>4</div>
+      </div>
+      <div className='bg-blue-200 transition-all hover:shadow-2xl hover:border border-black'>
+      <div className='w-[350px] h-[250px] bg-blue-300 flex justify-center items-center hover:text-[32px] border border-black hover:scale-95  transition-all		'>5</div>
+      </div>
+      <div className='bg-blue-200 transition-all hover:shadow-2xl hover:border border-black'>
+      <div className='w-[350px] h-[250px] bg-blue-300 flex justify-center items-center hover:text-[32px] border border-black hover:scale-95  transition-all		'>6</div>
+      </div>
+      <div className='bg-blue-200 transition-all hover:shadow-2xl hover:border border-black'>
+      <div className='w-[350px] h-[250px] bg-blue-300 flex justify-center items-center hover:text-[32px] border border-black hover:scale-95  transition-all		'>7</div>
+      </div>
+      <div className='bg-blue-200 transition-all hover:shadow-2xl hover:border border-black'>
+      <div className='w-[350px] h-[250px] bg-blue-300 flex justify-center items-center hover:text-[32px] border border-black hover:scale-95  transition-all		'>8</div>
+      </div>
+      <div className='bg-blue-200 transition-all hover:shadow-2xl hover:border border-black'>
+      <div className='w-[350px] h-[250px] bg-blue-300 flex justify-center items-center hover:text-[32px] border border-black hover:scale-95  transition-all		'>9</div>
+      </div>
+      <div className='bg-blue-200 transition-all hover:shadow-2xl hover:border border-black'>
+      <div className='w-[350px] h-[250px] bg-blue-300 flex justify-center items-center hover:text-[32px] border border-black hover:scale-95  transition-all		'>10</div>
+      </div>
+
+   </div>
+
+  )
+}
+
+export default App
